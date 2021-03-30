cdffa668c53eaf43690e1fb9d28eeeef2d65cc86
pigchen <wchen950527@gmail.com>
2021-03-30 19:34:46 +0800

    first commit

---
+++ src/main/java/com/pigchen/pigchen/PigchenApplication.java
@@ -0,0 +1,13 @@
+package com.pigchen.pigchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pigchen/pigchen/PigchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigchen.pigchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
